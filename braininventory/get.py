a22a203e28ce253b14d18fbc266e72f18b966e67
Ivan Cao-Berg <icaoberg@psc.edu>
2023-07-17 02:08:29 -0400

    Refactor variables, fix typos, update comments, remove unused code, improve error handling.

--- braininventory/get.py
+++ braininventory/get.py
@@ -5,10 +5,11 @@ from datetime import date
 
 link = "https://download.brainimagelibrary.org"
 
+
 def today():
     """
-	Get today's snapshot of Brain Image Library.
-	"""
+    Get today's snapshot of Brain Image Library.
+    """
 
     server = f"{link}/inventory/daily/reports/"
     filename = "today.json"
@@ -25,94 +26,108 @@ def today():
     else:
         print("Error: Failed to fetch JSON data")
         return pd.DataFrame()
-    
+
+
 def __get_number_of_datasets(df):
-     return len(df)
+    return len(df)
+
 
 def __get_completeness_score(df):
-      return df['score'].sum()/len(df)
+    return df["score"].sum() / len(df)
+
 
 def __is_reachable(url):
-	response = requests.get(url)
-
-	if response.status_code == 200:
-		return True
-	else:
-		return False
-
-def __get_metadata_version(df):
-    return df['metadata_version'] .value_counts().to_dict()
-
-def __get_contributor(df):
-    return df['contributor'].value_counts().to_dict()
-
-def __get_affilation(df):
-    return df['affiliation'].value_counts().to_dict()
-
-def __get_award_number(df):
-    return df['award_number'].value_counts().to_dict()
-
-def __get_species(df):
-    return df['species'].value_counts().to_dict()
-
-def __get_cnbtaxonomy(df):
-    return df['cnbtaxonomy'].value_counts().to_dict()
-
-def __get_samplelocalid(df):
-    return df['samplelocalid'].value_counts().to_dict()
-
-def __get_genotype(df):
-    return df['genotype'].value_counts().to_dict()
-
-def __get_generalmodality(df):
-    return df['generalmodality'].value_counts().to_dict()
-
-def __get_technique(df):
-    return df['technique'].value_counts().to_dict()
-
-def __get_locations(df):
-    return df['locations'].value_counts().to_dict()
-
-def __get_json_file(df):
-    """
-	Retrieve the json_file from the Brain Image Library of a dataset.
-	"""
-	
-    # Make sure there is data to request
-    assert df['score'].values[0] != 0
-
-    # Create working link
-    url = sample['json_file'].values[0].replace('/bil/data', link)
     response = requests.get(url)
 
-    return response.json()
+    if response.status_code == 200:
+        return True
+    else:
+        return False
+
+
+def __get_metadata_version(df):
+    return df["metadata_version"].value_counts().to_dict()
+
+
+def __get_contributor(df):
+    return df["contributor"].value_counts().to_dict()
+
+
+def __get_affilation(df):
+    return df["affiliation"].value_counts().to_dict()
+
+
+def __get_award_number(df):
+    return df["award_number"].value_counts().to_dict()
+
+
+def __get_species(df):
+    return df["species"].value_counts().to_dict()
+
+
+def __get_cnbtaxonomy(df):
+    return df["cnbtaxonomy"].value_counts().to_dict()
+
+
+def __get_samplelocalid(df):
+    return df["samplelocalid"].value_counts().to_dict()
+
+
+def __get_genotype(df):
+    return df["genotype"].value_counts().to_dict()
+
+
+def __get_generalmodality(df):
+    return df["generalmodality"].value_counts().to_dict()
+
+
+def __get_technique(df):
+    return df["technique"].value_counts().to_dict()
+
+
+def __get_locations(df):
+    return df["locations"].value_counts().to_dict()
+
+
+def __is_json_file_reachable(df):
+    """
+    Retrieve the json_file from the Brain Image Library of a dataset.
+    """
+
+    # Make sure there is data to request
+    if df["score"].values[0] != 0:
+        # Create working link
+        url = df["json_file"].values[0].replace("/bil/data", link)
+        response = requests.get(url)
+
+        return response.json()
+    else:
+        return None
+
 
 def report():
     # Get today's date
-	tdate = date.today()
+    tdate = date.today()
 
-	# Convert date to string
-	tdate = tdate.strftime("%Y-%m-%d")
-        
-	df = today()
+    # Convert date to string
+    tdate = tdate.strftime("%Y-%m-%d")
 
-	report = {}
-	report['date'] = tdate
-	report['number_of_datasets'] = __get_number_of_datasets(df)
-	report['completeness_score'] = __get_completeness_score(df)
-	report['metadata_version'] = __get_metadata_version(df)
-    report['contributor'] =  __get_contributor(df)
-    report['affiliation'] =  __get_affilation(df)
-    report['award_number'] = __get_award_number(df)
-    report['species'] = __get_species(df)
-    report['cnbtaxonomy'] = __get_cnbtaxonomy(df)
-    report['samplelocalid'] = __get_samplelocalid(df)
-    report['genotype'] = __get_genotype(df)
-    report['generalmodality'] = __get_generalmodality(df)
-    report['technique'] = __get_technique(df)
-    report['locations'] = __get_locations(df)
-     
-	report['is_reachable'] = df['URL'].apply(__is_reachable)
+    df = today()
 
-	return report
+    report = {}
+    report["date"] = tdate
+    report["number_of_datasets"] = __get_number_of_datasets(df)
+    report["completeness_score"] = __get_completeness_score(df)
+    report["metadata_version"] = __get_metadata_version(df)
+    report["contributor"] = __get_contributor(df)
+    report["affiliation"] = __get_affilation(df)
+    report["award_number"] = __get_award_number(df)
+    report["species"] = __get_species(df)
+    report["cnbtaxonomy"] = __get_cnbtaxonomy(df)
+    report["samplelocalid"] = __get_samplelocalid(df)
+    report["genotype"] = __get_genotype(df)
+    report["generalmodality"] = __get_generalmodality(df)
+    report["technique"] = __get_technique(df)
+    report["locations"] = __get_locations(df)
 
+    return report
