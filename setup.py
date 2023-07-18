33117e5371779b66f20cd9fdbb05e2204cf0d307
Togundar <138719368+Togundar@users.noreply.github.com>
2023-07-18 15:52:25 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,3 +20,38 @@ setuptools.setup(
     ],
     python_requires=">=3.6",
 )
+import pandas as pd
+import urllib.request
+
+url = 'https://download.brainimagelibrary.org/inventory/daily/reports/today.json'
+file_path, _ = urllib.request.urlretrieve(url)
+df = pd.read_json(file_path)
+
+#get affiliations
+affiliations = df['genotype'].value_counts().to_dict()
+affiliations
+
+!pip install kaleido
+
+import plotly.graph_objects as go
+from datetime import date
+
+def create_tree_map(frequency_dict):
+    labels = list(frequency_dict.keys())
+    values = list(frequency_dict.values())
+
+    fig = go.Figure(go.Treemap(
+        labels=labels,
+        parents=[''] * len(labels),
+        values=values,
+        textinfo='label+value'
+    ))
+
+    fig.update_layout(title='Genotypes')
+
+    today = date.today()
+    output_path = f'treemap-{today.strftime("%Y%m%d")}.png'
+    fig.write_image(output_path)
+    fig.show()
+
+create_tree_map(affiliations)
